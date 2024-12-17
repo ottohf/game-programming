710e768cd9e755f96b06c73a657f71ec887f54b1
ottohf <otto.fearnley@gmail.com>
2024-12-17 23:02:32 +0900

    sound effect bullet

--- Assets/Scripts/ContactDestroyer.cs
+++ Assets/Scripts/ContactDestroyer.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class ContactDestroyer : MonoBehaviour
 {
+    private AudioSource audioSource;//for hit sound
+    public AudioClip soundEffect1;
+    public AudioClip soundEffect2;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Enemy") || other.gameObject.layer == LayerMask.NameToLayer("Player") || other.gameObject.layer == LayerMask.NameToLayer("PlayerBase"))
         {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Player")){
+                audioSource.PlayOneShot(soundEffect2); // Player gets hit
+            }
+            else
+            {
+                audioSource.PlayOneShot(soundEffect1); // Enemy gets hit
+            }
             var life = other.gameObject.GetComponent<Life>();
             if (life != null)
             {
